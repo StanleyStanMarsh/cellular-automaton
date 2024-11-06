3ae3aaa00a3cac8535715e25b19e4f2d478d545a
StanleyStanMarsh <iastafev56@gmail.com>
2024-11-06 20:33:07 +0300

    feat: add cell next state without handling cells on the edge

--- grid.py
+++ grid.py
@@ -29,6 +29,9 @@ class Cell:
     def __init__(self, state: bool):
         self.state = state
 
+    def new_state(self, state: bool):
+        self.state = state
+
 
 class Grid:
     def __init__(self, rows: int, cols: int):
@@ -43,3 +46,15 @@ class Grid:
         bin_rule_value = format(initial_rule_value, f'0{constant.NUM_OF_DIGITS}b')
         string_result = bin_rule_value[num_of_state]
         return str_to_bool(string_result)
+
+    def __cell_next_state(self, row: int, col: int):
+        if 0 < row < self.rows - 1 and 0 < col < self.cols - 1:
+            s_0 = self.grid[row, col]
+            s_1 = self.grid[row + 1, col]
+            s_2 = self.grid[row - 1, col]
+            s_3 = self.grid[row, col - 1]
+            s_4 = self.grid[row, col + 1]
+            moore_neighbourhood = (s_0.state, s_1.state, s_2.state, s_3.state, s_4.state)
+            self.grid[row, col].new_state(self.__rule(moore_neighbourhood))
+        else:
+            pass
