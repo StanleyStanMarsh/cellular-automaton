9c9f635bf3776e60d9657d43ade64044baea5098
StanleyStanMarsh <iastafev56@gmail.com>
2024-11-06 19:19:44 +0300

    feat: add Grid class

--- grid.py
+++ grid.py
@@ -1,6 +1,16 @@
+import numpy as np
+
+
 class Cell:
     def __init__(self):
         self.state = False
 
     def __init__(self, state: bool):
         self.state = state
+
+
+class Grid:
+    def __init__(self, rows: int, cols: int):
+        self.rows = rows
+        self.cols = cols
+        self.grid = np.empty((rows, cols), dtype=Cell)
